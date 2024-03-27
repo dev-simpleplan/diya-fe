603c5825bf244348563bee6a321448b033458f63
Anshul Pradhan <anshulpradhan1@gmail.com>
2024-03-27 19:02:39 +0530

    Hi
    Hi

--- js/custom.js
+++ js/custom.js
@@ -48,74 +48,74 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-// gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
 
-// let navigation = document.querySelector(".navigation");
-// let images = document.querySelectorAll(".bannerimg img");
-// let currentImageIndex = 0; // Track the index of the currently displayed image
+let navigation = document.querySelector(".navigation");
+let images = document.querySelectorAll(".bannerimg img");
+let currentImageIndex = 0; // Track the index of the currently displayed image
 
-// // Hide the navigation initially
-// navigation.style.display = "none";
+// Hide the navigation initially
+navigation.style.display = "none";
 
-// let t1 = gsap.timeline({
-//     scrollTrigger: {
-//         trigger: ".banner",
-//         pin: true,
-//         start: "top top",
-//         end: "+=150%",
-//         scrub: 0.1,
-//         markers: true,
-//         ease: "none",
-//     },
-// });
+let t1 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".banner",
+        pin: true,
+        start: "top top",
+        end: "+=150%",
+        scrub: 0.1,
+        markers: true,
+        ease: "none",
+    },
+});
 
-// t1.fromTo(
-//     ".bannertext",
-//     { y: 315, scale: 1 },
-//     { y: -95, scale: 0.6 },
-//     "abc"
-// )
-// .fromTo(
-//     ".bannertext",
-//     { x: "0" },
-//     { x: "-240" },
-// )
-// .fromTo(
-//     ".bannerimg",
-//     { width: "100vw", height: "100vh", top: 0, left: 0 },
-//     { width: 140, height: 70, top: "40%", left: "41%" },
-//     "abc"
-// )
-// .to(".text1 , .text2 ,.bb-left ,.bb-right", { y: 0, x: 0, duration: 0.4 });
+t1.fromTo(
+    ".bannertext",
+    { y: 315, scale: 1 },
+    { y: -95, scale: 0.6 },
+    "abc"
+)
+.fromTo(
+    ".bannertext",
+    { x: "0" },
+    { x: "-240" },
+)
+.fromTo(
+    ".bannerimg",
+    { width: "100vw", height: "100vh", top: 0, left: 0 },
+    { width: 140, height: 70, top: "40%", left: "41%" },
+    "abc"
+)
+.to(".text1 , .text2 ,.bb-left ,.bb-right", { y: 0, x: 0, duration: 0.4 });
 
-// // ScrollTrigger to show/hide navigation
-// ScrollTrigger.create({
-//     trigger: ".banner",
-//     start: "top top",
-//     end: "+=150%",
-//     onEnter: () => {
-//         navigation.style.display = "block";
-//     },
-//     onLeaveBack: () => {
-//         navigation.style.display = "none";
-//     }
-// });
+// ScrollTrigger to show/hide navigation
+ScrollTrigger.create({
+    trigger: ".banner",
+    start: "top top",
+    end: "+=150%",
+    onEnter: () => {
+        navigation.style.display = "block";
+    },
+    onLeaveBack: () => {
+        navigation.style.display = "none";
+    }
+});
 
-// // ScrollTrigger to change images
-// ScrollTrigger.create({
-//     trigger: ".banner",
-//     start: "top top",
-//     end: "+=150%",
-//     onUpdate: (self) => {
-//         let progress = self.progress;
-//         let index = Math.floor(progress * images.length);
+// ScrollTrigger to change images
+ScrollTrigger.create({
+    trigger: ".banner",
+    start: "top top",
+    end: "+=150%",
+    onUpdate: (self) => {
+        let progress = self.progress;
+        let index = Math.floor(progress * images.length);
         
-//         // Check if index has changed
-//         if (index !== currentImageIndex) {
-//             // Change the source of the corresponding image
-//             images[currentImageIndex].style.opacity = 0; // Hide previous image
-//             images[index].style.opacity = 1; // Show current image
-//             currentImageIndex = index; // Update current index
-//         }
-//     }
-// });
+        // Check if index has changed
+        if (index !== currentImageIndex) {
+            // Change the source of the corresponding image
+            images[currentImageIndex].style.opacity = 0; // Hide previous image
+            images[index].style.opacity = 1; // Show current image
+            currentImageIndex = index; // Update current index
+        }
+    }
+});
