53adbc60c68203536ce97230dce4f85284de96b2
Kimreet <kimreetkaur16@gmail.com>
2024-04-05 19:08:37 +0530

    commit

--- js/custom.js
+++ js/custom.js
@@ -81,38 +81,57 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 //   counter js
-    var counted = 0;
-      $(window).scroll(function() {
+    var counted1 = 0;
+var counted2 = 0;
 
-        var oTop = $('.inner-acc').offset().top - window.innerHeight;
-        if (counted == 0 && $(window).scrollTop() > oTop) {
-          $('span.num-counter').each(function() {
-            var $this = $(this),
-              countTo = $this.attr('data-count');
-            $({
-              countNum: $this.text()
-            }).animate({
-                countNum: countTo
-              },
-
-              {
-
-                duration: 2000,
-                easing: 'swing',
-                step: function() {
-                  $this.text(Math.floor(this.countNum));
-                },
-                complete: function() {
-                  $this.text(this.countNum);
-                  //alert('finished');
-                }
-
-              });
-          });
-          counted = 1;
+$(window).scroll(function() {
+  // First section
+  var oTop1 = $('.inner-acc:first').offset().top - window.innerHeight;
+  if (counted1 == 0 && $(window).scrollTop() > oTop1) {
+    $('.inner-acc:first span.num-counter').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({ countNum: $this.text() }).animate(
+        { countNum: countTo },
+        {
+          duration: 2000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+          }
         }
+      );
+    });
+    counted1 = 1;
+  }
 
-      });
+  // Second section
+  var oTop2 = $('.inner-acc:last').offset().top - window.innerHeight;
+  if (counted2 == 0 && $(window).scrollTop() > oTop2) {
+    $('.inner-acc:last span.num-counter').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({ countNum: $this.text() }).animate(
+        { countNum: countTo },
+        {
+          duration: 2000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+          }
+        }
+      );
+    });
+    counted2 = 1;
+  }
+});
+
 // counter js ends
 //Faq Accordian
 $(document).ready(function() {
